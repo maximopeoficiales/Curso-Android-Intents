324fb4a4a2efddbddd82374281454852a944ec6d
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-04-08 01:27:54 -0500

    se agrego intents implicitos : contact,email fast,call phone marked,

--- app/src/main/java/com/maximoprog/seccion_diseno/ThirdActivity.java
+++ app/src/main/java/com/maximoprog/seccion_diseno/ThirdActivity.java
@@ -24,13 +24,16 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_third);
+
         binding = ActivityThirdBinding.inflate(getLayoutInflater());
         //se carga la vista con el binding
         setContentView(binding.getRoot());
+
+//        boton para hacer llamadas
         binding.imageButtonPhone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String phoneNumber = binding.editTextNumber.getText().toString();
+                String phoneNumber = binding.txtPhone.getText().toString();
                 if (phoneNumber != null && !phoneNumber.isEmpty()) {
 //                    Comprobar version actual de android estamos corriendo
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -86,6 +89,79 @@ public class ThirdActivity extends AppCompatActivity {
 
             }
         });
+
+
+//        boton para la direccion web
+        binding.imageButtonWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = binding.txtWeb.getText().toString();
+                if (url != null && !url.isEmpty()) {
+                    Intent intentWeb = new Intent(Intent.ACTION_VIEW, Uri.parse("http://" + url));
+                    /*intentWeb.setAction(Intent.ACTION_VIEW);
+                    intentWeb.setData(Uri.parse("http://" + url));*/
+                    startActivity(intentWeb);
+                } else {
+                    sayMessage("Por favor Rellenar el campo url");
+                }
+            }
+        });
+//         boton marcar al telefono pero no llamar
+        binding.imageButtonPhoneMarcar2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String numberPhone = binding.txtPhone2.getText().toString();
+                if (numberPhone != null && !numberPhone.isEmpty()) {
+                    Intent intentPhone = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + numberPhone));
+                    intentPhone.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intentPhone.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    intentPhone.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                    startActivity(intentPhone);
+                } else {
+                    sayMessage("Por favor Rellenar el campo del telefono a marcar");
+                }
+            }
+        });
+//        enviar correo de manera implicita
+        binding.imageButtonSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = binding.txtEmail.getText().toString();
+                if (email != null && !email.isEmpty()) {
+                    Intent intentEmail = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + email));
+                    startActivity(intentEmail);
+                } else {
+                    sayMessage("Por favor Rellenar el campo email");
+                }
+            }
+        });
+//        abrir aplicacion de contactos
+        binding.imageButtonContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("content://contacts/people")));
+            }
+        });
+//        intent Explicito porque tambien se puede hacer explicito espefico con que app quieres que se habra
+        binding.imageButtonSendEmailBody.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent iEmail = new Intent(Intent.ACTION_SEND);
+                iEmail.setType("message/rfc822");
+                iEmail.putExtra(Intent.EXTRA_SUBJECT, "Titulo de Email");
+                iEmail.putExtra(Intent.EXTRA_TEXT, "Hola bro esto es un correo");
+                iEmail.putExtra(Intent.EXTRA_EMAIL, new String[]{"maximopeoficiales1@gmail.com", "maximopeoficiales2@gmail.com"});
+                iEmail.setPackage("com.google.android.gm");
+                if (iEmail.resolveActivity(getPackageManager()) != null) {
+                    startActivity(iEmail);
+//                startActivity(Intent.createChooser(iEmail, "Elige cliente de correo"));
+                } else {
+                    sayMessage("Gmail App is not installed");
+                }
+            }
+        });
+
     }
 
     //este metodo se ejecuta cada vez que solicita un permiso
@@ -99,7 +175,7 @@ public class ThirdActivity extends AppCompatActivity {
 //                    comprobar si ha sido aceptadod o denegado la peticion del permiso
                     if (result == PackageManager.PERMISSION_GRANTED) {
 //                            Concedio su permiso
-                        Intent intentCall = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + binding.editTextNumber.getText().toString()));
+                        Intent intentCall = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + binding.txtPhone.getText().toString()));
 //                        si el usuario rechaza el permiso sale del switch
                         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
                             return;
